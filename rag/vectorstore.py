b39b1aee71b84db16e8844fd31bdb22b784f727c
jayantdeshwal <jayant.deshwal.56@gmail.com>
2026-07-14 10:06:34 +0530

    Fix RAG

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -7,11 +7,19 @@ from rag.embeddings import (
 
 def create_vectorstore(chunks):
 
+    print("Before get_embedding_model()")
+
     embeddings = get_embedding_model()
+
+    print("After get_embedding_model()")
+
+    print("Before FAISS")
 
     vectorstore = FAISS.from_documents(
         chunks,
         embeddings
     )
+
+    print("After FAISS")
 
     return vectorstore
