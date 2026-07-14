2bbf44bb24d654f30c73ad6e8d8c46624e2936c7
jayantdeshwal <jayant.deshwal.56@gmail.com>
2026-07-14 10:04:44 +0530

    Fix RAG

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,9 +1,18 @@
+import time
 import streamlit as st
 from langchain_huggingface import HuggingFaceEmbeddings
 
 @st.cache_resource
 def get_embedding_model():
 
-    return HuggingFaceEmbeddings(
+    print("Embedding loading started")
+
+    start = time.time()
+
+    model = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
+
+    print(f"Embedding loaded in {time.time()-start:.2f} seconds")
+
+    return model
