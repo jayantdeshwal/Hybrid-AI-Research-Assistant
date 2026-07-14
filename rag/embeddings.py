9fc999054cb87e293c808eca7c5a154b7ec6c894
jayantdeshwal <jayant.deshwal.56@gmail.com>
2026-07-14 09:49:07 +0530

    Fix RAG

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,10 +1,9 @@
+import streamlit as st
 from langchain_huggingface import HuggingFaceEmbeddings
 
-
+@st.cache_resource
 def get_embedding_model():
 
-    embeddings = HuggingFaceEmbeddings(
+    return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
-
-    return embeddings
